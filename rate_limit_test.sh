86d3600f44edccf7e317ef0e565bdc29aabf5da0
ecokova <elena.cokova@tufts.edu>
2018-04-17 17:08:23 -0400

    Tweaked benchmark scripts and started writing requirements txt file

--- rate_limit_test.sh
+++ rate_limit_test.sh
@@ -2,29 +2,46 @@
 #  $2 = file containing list of files to use as samples 
 
 timestamp=`date +%m-%d_%H%M`
-
 for ((j=0;j<$1;j++))
 do
     for i in 1000000000,1500000000 100000000,150000000 10000000,15000000 1000000,1500000; do 
-        while read -r line
+        IFS=',' read rate cap <<< "${i}"
+        echo "${rate}" bandwidth and "${cap}" burst rate, iteration $j
+        while read -r file
         do
-            echo $file
-            IFS=',' read rate cap <<< "${i}"
-            echo "${rate}" bandwidth and "${cap}" burst rate, iteration $j
-            echo "Sample $j" >> ${timestamp}_${rate}_${cap}.dat
-            echo "${rate}" bandwidth and "${cap}" burst rate >> ${timestamp}_${rate}_${cap}.dat
+            echo $file            
+            echo "File ${file}, sample $j, bandwidth= ${rate} burst rate= ${cap}" >> ${timestamp}_${rate}_${cap}.dat
+
             `python -m SimpleHTTPServer >& /dev/null & `
             sleep 1
             echo Started http server
             `python proxy.py 8080 -r -v --bandwidth=${rate} --burst_rate=${cap} >& proxy_${timestamp}_${file}_${j}.log & `
             sleep 2
             echo Started proxy
-            echo "Timing results:" >> ${timestamp}_${rate}_${cap}.dat
-            `python run_tests.py --num_samples 1 --files $file >& run_tests_output`
-            cat run_tests_output >> ${timestamp}_${rate}_${cap}.dat
-            sleep 1
-            echo Done running script, killing all python now
+    
+            /usr/bin/time -a -o ratelimit_times_${file}_${timestamp}_${rate}_${cap}.dat -p curl --proxy localhost:8080 localhost:8000/$file > /dev/null
+            echo Done with curl, killing proxy now
             killall python
-        done < "$1"
+        done < "$2"
     done
 done
+
+for i in 1000000000,1500000000 100000000,150000000 10000000,15000000 1000000,1500000; do 
+    IFS=',' read rate cap <<< "${i}"
+    echo "${rate}" bandwidth and "${cap}" burst rate
+    echo "${rate}" bandwidth and "${cap}" burst rate >> final_ratelimit_time_report_${timestamp}.txt
+    while read -r line
+    do
+        echo $line >> final_ratelimit_time_report_${timestamp}.txt
+        
+        filename=ratelimit_times_${line}_${timestamp}_${rate}_${cap}.dat
+        echo "Calculating mean and stdev for ${line} (bw=${rate}, burst=${cap}"
+        ./get_avg_time.sh ${filename} >> final_ratelimit_time_report_${timestamp}.txt
+    done < "$2"
+done
+
+cat final_ratelimit_time_report_${timestamp}.txt
+
+mkdir timing_stats_ratelimit_${timestamp}
+`mv ratelimit_times_*.dat timing_stats_ratelimit_${timestamp}`
+`cp final_ratelimit_time_report_${timestamp}.txt timing_stats_ratelimit_${timestamp}`
